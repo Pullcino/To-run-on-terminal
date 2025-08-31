4886b9faeb49b1093396a61490a3a6b095a8cba3
Arthur Pulcino <apulcino8@gmail.com>
2025-08-31 15:38:58 -0300

    finally i finished
    the code is completed

--- random number game/random number.py
+++ random number game/random number.py
@@ -4,23 +4,25 @@ number = random.randint(1, 10)
 
 trys = 3
 
-guess = int(input(f'Try to guess the number between 1 and 10\nyou have {trys} attemps: '))
+while trys != 0:
 
-while trys > 0:
-    
+    guess = int(input(f'Try to guess the number between 1 and 10\nyou have {trys} attemps: '))
+
     if guess != number:
 
         trys = trys - 1
 
-        guess = int(input(f'Try to guess the number between 1 and 10\nyou have {trys} attemps: '))
-
     elif guess == number:
 
         print('You win')
-    
-    elif trys == 0:
 
-        print('you lose')
         break
+
+    if trys == 0:
+
+        print('You lose')
+
+print('Game over')
+        
 
 
