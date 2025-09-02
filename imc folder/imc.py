070f87a186db848cde7949207af860a8a2a0443b
Arthur Pulcino <apulcino8@gmail.com>
2025-09-01 22:41:58 -0300

    finally finisheeeeeed

--- imc folder/imc.py
+++ imc folder/imc.py
@@ -1,13 +1,60 @@
-print('Body mass index (BMI) is a simple way to identify weight-related problems, such as obesity or malnutrition, in children, adolescents, adults, and the elderly.In addition to calculating your BMI, this calculator also indicates the ideal weight range you should have to ensure a better quality of life. Maintaining a weight within the ideal range helps prevent the onset of several chronic diseases, such as diabetes and hypertension.Enter your data into the calculator and calculate your BMI:')
+print('Body mass index (BMI) is a simple way to identify weight-related problems, such as obesity or malnutrition, in children, adolescents, adults, and the elderly.\nIn addition to calculating your BMI, this calculator also indicates the ideal weight range you should have to ensure a better quality of life.\nMaintaining a weight within the ideal range helps prevent the onset of several chronic diseases, such as diabetes and hypertension.\nEnter your data into the calculator and calculate your BMI:')
 
 height = float(input('What is your height : '))
 
-if len(height) == 3 :
+type_ = input('Is the measurement in feet or meters : ')
 
-    height = height * 30.48
+if type_ == 'feet':
 
-weight = float(input('What is you weight : '))
+    height = height * 0.3048
 
-result = weight / (height*height)
+    print('Next question')
 
-print(result)
+
+else:
+
+    print('Next question')
+
+weight = float(input('What is your weight : '))
+
+type_2 = input('Is the measurement pounds or kilogram : ')
+
+if type_2 == 'pounds':
+
+    convert_2 = weight * 0.45
+
+    result = weight / (height*height)
+
+    print(f'Your BMI is {result}')
+
+else:
+
+    result = weight / (height*height)
+
+    print(f'Your BMI is {result}')
+
+print('See the table below :\nBMI less than 18,5 = thinness\nBMI between 18,5 and 24,9 = Normal\nBMI between 25 and 29,9 = overweight\nBMI between 30,0 and 34,9 = Grade I obesity\nBMI between 35 and 39,9 = Grade II obesity\nBMI Greater than 40 = Grade III obesity ')
+
+if result < 18.5:
+
+    print('Your situation is thinness')
+
+elif result >= 18.5 and result <= 24.9:
+
+    print('Your situation is normal')
+
+elif result >= 25 and result <=29.9:
+
+    print('Your situation is overweight')
+
+elif result >= 30 and result <=34.9:
+
+    print('Your situation is Grade I obesity')
+
+elif result >= 35 and result <=39.9:
+
+    print('Your situation is Grade II obesity')
+
+else:
+
+    print('Your situation is Grade III obesity')
