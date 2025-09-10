6cd48dec7e3887eb2594d30739deee4d8958b9b9
Arthur Pulcino <apulcino8@gmail.com>
2025-09-10 00:24:08 -0300

    coding
    At the moment I have a problem, the code does not show errors, however, the list does not appear in the terminal.

--- Shopping list.py/shoppinglist.py
+++ Shopping list.py/shoppinglist.py
@@ -1,3 +1,36 @@
-To_buy = []
+to_buy = []
 
-question = int(input("welcome to the shopping list choose one option \n1 - add item to list\n2 - "))
+question = int(input("welcome to the shopping list choose one option \n1 - add item to list\n2 - Remove item from list\n3 - Display shopping list\n4 - Leave shopping list\n"))
+
+while question != 4:
+
+    if question == 1:
+
+        item = input("Enter the desired item: ")
+
+        to_buy = item.join(to_buy)
+
+        question = int(input("Choose one option \n1 - add item to list\n2 - Remove item from list\n3 - Display shopping list\n4 - Leave shopping list\n"))
+
+    elif question == 2:
+
+        print(f"You have this itens on the list {to_buy}")
+
+        item = input("Enter the desired item you wanna remove: ")
+
+        to_buy.remove(item)
+
+        question = int(input("Choose one option \n1 - add item to list\n2 - Remove item from list\n3 - Display shopping list\n4 - Leave shopping list\n"))
+
+    elif question == 3:
+
+        print(f"That is your shopping list {to_buy}")
+
+        question = int(input("Choose one option \n1 - add item to list\n2 - Remove item from list\n3 - Display shopping list\n4 - Leave shopping list\n"))
+
+    elif question == 4:
+
+        break
+
+print('You leave the shopping list')
+
