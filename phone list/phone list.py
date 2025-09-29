9db6b7da746ba8d99d03a1c82ed4d6cc926a22e4
Arthur Pulcino <apulcino8@gmail.com>
2025-09-28 23:32:44 -0300

    the change contact function was created

--- phone list/phone list.py
+++ phone list/phone list.py
@@ -14,6 +14,50 @@ def add_contact(contacts):
 
     contacts[name] = number
 
+    print("Contact added with sucess")
+
+def change_contact(contacts):
+
+    actual_name = input("Digit the name of the contact you wanna change")
+
+    if actual_name in contacts:
+
+        new_name = input("Digit the new name (press enter without typing to keep the current name): ")
+        new_number = input("Digit the new number (press enter without typing to keep the current name): ")
+
+        if new_name :
+
+            if new_name in contacts:
+
+                print("\nThis contact already exist try another name ")
+
+                return
+            
+            contacts[new_name] = contacts[actual_name]
+
+            del contacts[actual_name]
+
+        else:
+
+            new_name = actual_name
+
+        if new_number:
+
+            contacts[new_name] = new_number
+
+        print("Contact chaged with sucess")
+
+    else:
+
+        print("This contact dont exist")
+
+
+def remove_contact(contacts):
+
+    question = input("Which contact")
+
+
+
 def main():
 
     contacts = {}
@@ -27,6 +71,12 @@ def main():
         if option == "1":
 
             add_contact(contacts)
+
+        elif option == "2":
+
+            change_contact(contacts)
+
+main()
 
         
 
