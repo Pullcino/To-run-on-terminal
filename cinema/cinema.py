740a81384e053704c8e800fe3231b71faac64481
Arthur Pulcino <apulcino8@gmail.com>
2025-11-03 19:03:48 -0300

    finished

--- cinema/cinema.py
+++ cinema/cinema.py
@@ -13,6 +13,40 @@ for line in range(4):
 
 while True:
 
-    print()
-    print()
-    print()
+    print("Cinema")
+    print("1 - View the seats")
+    print("2 - Choose one seat")
+    print("3 - Leave the cinema")
+
+    choose = int(input("Choose one between the options: (1 - 3)"))
+
+    if choose == 1:
+
+        for line in cinema:
+
+            for seat in line:
+
+                print(seat, end=" ")
+
+            print()
+
+    elif choose == 2:
+
+        line = int(input("Digit the number of the line of your seat (0 - 4)")) 
+        seat = int(input("Digit the number of your seat (0 - 9)"))
+
+        cinema[line][seat] = "B"
+
+        for line in cinema:
+
+            for seat in line:
+
+                print(seat, end=" ")
+
+            print()
+
+    elif choose == 3:
+
+        break
+
+print("You leave the program")
