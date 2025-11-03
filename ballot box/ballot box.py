d407742b20295fb9ad4a53eb56d20f1b05b96c0d
Arthur Pulcino <apulcino8@gmail.com>
2025-11-03 18:58:14 -0300

    Start the cinema simulator

--- ballot box/ballot box.py
+++ ballot box/ballot box.py
@@ -8,10 +8,8 @@ for line in range(4):
 
     vote = input("Digit the name of the candidate you wanna vote: (Arthur, Emily, Alice)")
 
-    for candidate in urna:
+    encontrado = False
 
-        encontrado = False
-    
     for candidato in urna:
         
         if candidato[0] == vote:
@@ -24,5 +22,19 @@ for line in range(4):
     if not encontrado:
         print("Voto nulo.")
 
-print("\n Results: ")
+maximum_votes = -1
+winner = ""
+
+for candidato in urna:
+
+    print(f"{candidato[0]}: {candidato[1]} votos")
+
+    if candidato[1] > maximum_votes:
+
+        maximum_votes = candidato[1]
+        
+        winner = candidato[0]
+
+print(f"The winner is : {winner} com {maximum_votes} votes")
+
 
---
+++ cinema/cinema.py
@@ -0,0 +1,18 @@
+cinema = []
+
+for line in range(4):
+
+    seat = []
+
+    for column in range(10):
+
+        seat.append("A")
+
+    cinema.append(seat)
+
+
+while True:
+
+    print()
+    print()
+    print()
