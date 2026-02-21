d3b17db0b1dc156c9a0e05027a9ef725d2685443
Arthur Pulcino <apulcino8@gmail.com>
2026-02-20 21:53:07 -0300

    I forgot to iterate.

--- until-zero/calculator-until-zero.py
+++ until-zero/calculator-until-zero.py
@@ -1,3 +1,15 @@
 number = int(input("Digit a number: "))
 
 print("Now we gonna subtract until this number turn to zero")
+
+def sub(a, b):
+
+    return a - b
+
+while number > 0:
+
+    new_number = int(input("Enter the number you want to subtract."))
+
+    print(sub(number, new_number))
+
+    number
